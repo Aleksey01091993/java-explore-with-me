a7fa740266d182184146748c0bd4ade1f146e801
Aleksey01091993 <AShvedov1993@yandex.ru>
2024-02-18 11:38:29 +0300

    начало

---
+++ TheFunctionalityOfTheStatisticsService/src/main/java/ru/practicum/StatisticsService.java
@@ -0,0 +1,12 @@
+package ru.practicum;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsService {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsService.class, args);
+    }
+}
